e7196658f666e278bcc2874cde2e557177804b85
Joshua Garner <josh.garner94@gmail.com>
2017-12-01 11:24:40 -0800

    Updated OpenLog Library to 0.1.5

--- src/me/jgappsandgames/Build.java
+++ src/me/jgappsandgames/Build.java
@@ -1,6 +1,6 @@
 package me.jgappsandgames;
 
 public class Build {
-    public static final int build = 2;
-    public static final String version = "0.1.1";
+    public static final int build = 4;
+    public static final String version = "0.1.3";
 }
--- src/me/jgappsandgames/Main.java
+++ src/me/jgappsandgames/Main.java
@@ -1,14 +1,18 @@
 package me.jgappsandgames;
 
+import me.jgappsandgames.openlog.*;
 import me.jgappsandgames.openlog.Build;
 import me.jgappsandgames.openlog.Error;
 import me.jgappsandgames.openlog.Exception;
-import me.jgappsandgames.openlog.Log;
-import me.jgappsandgames.openlog.OpenLog;
 
 public class Main {
     public static void main(String[] args) {
+        FileWriter writer = FileWriter.getInstance();
+
         OpenLog.init();
+        Log.writers.add(writer);
+        Exception.writers.add(writer);
+        Error.writers.add(writer);
 
         // Information
         Log.i("OpenLog", String.valueOf(Build.build));
