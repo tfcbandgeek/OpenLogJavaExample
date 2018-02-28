6b9a5b5e089a74c38348b398c1e9c1d0d90ebcca
Joshua Garner <josh.garner94@gmail.com>
2018-02-27 21:18:20 -0800

    Updated Library to 1.3.0

--- src/me/jgappsandgames/Build.java
+++ src/me/jgappsandgames/Build.java
@@ -1,6 +1,6 @@
 package me.jgappsandgames;
 
 public class Build {
-    public static final int build = 7;
-    public static final String version = "0.3.1";
+    public static final int build = 8;
+    public static final String version = "1.3.0";
 }
